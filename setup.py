53fb1ec576b23f462db42349613f314da196c788
Thomas Eddenburg <florian.drux@rwth-aachen.de>
2018-09-16 13:44:47 +0200

    renamed setup.py name
    uploaded to https://pypi.org/project/GraphWFC/

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='GraphWaveFunctionCollapse',
-    version='0.9.0',
+    name='GraphWFC',
+    version='0.9.2',
+	author="lamelizard",
+    author_email="florian.drux@rwth-aachen.de",
     description='Colors a networkx (Di)Graph based on patterns extracted from an colored example (Di)Graph',
     packages=['graphwfc',],
     install_requires=[
@@ -11,5 +13,5 @@ setup(
     python_requires=">=3.0",
     url='https://github.com/lamelizard/GraphWaveFunctionCollapse',
     license='MIT',
-    long_description=open('README.md').read(),
+    long_description="A python 3.x package to color a graph based on patterns extracted from an colored example graph. More infos on https://github.com/lamelizard/GraphWaveFunctionCollapse",
 )
